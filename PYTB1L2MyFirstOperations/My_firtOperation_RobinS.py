6edd767edb989794b3b97595a6b73ae495a6c6ad
Streats22 <70856472+Streats22@users.noreply.github.com>
2020-10-01 14:33:08 +0200

    First operation

---
+++ PYTB1L2MyFirstOperations/My_firtOperation_RobinS.py
@@ -0,0 +1,29 @@
+##Om Functies te oefenne heb ik alle sommen in een fuctie gezet :)
+#De laatste 2 zijn alleen met een Var gedaan.
+def Optellen(varA, varB):
+    print("Optellen", varA + varB)
+
+Optellen(50, 20)
+
+def Aftrekken(varA, varB):
+    print("Aftrekken",varA - varB)
+Aftrekken(500, 70)
+
+
+def Delen(varA, varB):
+    print("Delen",varA / varB)
+Delen(250, 2)
+
+def RestW(varA, varB):
+    print("Modulo/RestWaarde",varA % varB)
+
+RestW(25, 20)
+
+F = 2
+F =+ 2
+
+print("Optellen bij Waarde",F)
+
+
+F /= 2
+print("Delen Door Waarde",F)
