367e2e70692a88576b13dfebaee36a5d973b2859
Streats22 <70856472+Streats22@users.noreply.github.com>
2020-10-30 14:11:04 +0100

    Aanpassingen

--- PYTB1L5Scooter /scooter_kosten.py
+++ PYTB1L5Scooter /scooter_kosten.py
@@ -1,7 +1,8 @@
 import time
+
 print("Calculatie voor hoeveel je spendeerd aan je maandlasten...")
-def bereken_maandkosten():
-    km_per_maand = float(input("Schrijf hoeveel KM je heb greden\n"))
+def bereken_maandkosten(km):
+    km_per_maand = km
     liter_per_kilometer = 0.4
     benzine_kosten_per_liter = 1.94
     verzekering_per_maand = 38
@@ -9,6 +10,6 @@ def bereken_maandkosten():
     time.sleep(1.2)
     print(".....calulating......")
     time.sleep(2)
-    print("€" + str(maandkosten))
+    return maandkosten
 
-bereken_maandkosten()
+print(bereken_maandkosten(int(input("Hoeveel KM rij je pm? "))))
