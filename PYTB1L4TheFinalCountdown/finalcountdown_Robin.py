4314479212a6dcbfe45798b098e2d4ee689a90f3
Streats22 <70856472+Streats22@users.noreply.github.com>
2020-10-30 14:11:28 +0100

    Aanpsassingen

--- PYTB1L4TheFinalCountdown/finalcountdown_Robin.py
+++ PYTB1L4TheFinalCountdown/finalcountdown_Robin.py
@@ -7,7 +7,6 @@ while X > 0:
     X -= 10
     print(X)
     time.sleep(0.2)
-    if X == 0:
-        print("Hoera! we zijn van 1000 tot 1")
-        webbrowser.open('https://www.youtube.com/watch?v=2Bjy5YQ5xPc')
-        break
+
+print("Hoera! we zijn van 1000 tot 1")
+webbrowser.open('https://www.youtube.com/watch?v=2Bjy5YQ5xPc')
